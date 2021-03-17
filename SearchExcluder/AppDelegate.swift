1bcdcad0689ac30e004e1bd8a0181e016f00d7a6
tkkrr <reioffitial@gmail.com>
2021-03-17 13:52:35 +0900

    Initial Commit

---
+++ SearchExcluder/AppDelegate.swift
@@ -0,0 +1,25 @@
+//
+//  AppDelegate.swift
+//  SearchExcluder
+//
+//  Created by 高倉礼 on 2021/03/17.
+//
+
+import Cocoa
+
+@main
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    func applicationDidFinishLaunching(_ notification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ notification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+    func applicationShouldTerminateAfterLastWindowClosed(_ sender: NSApplication) -> Bool {
+        return true
+    }
+
+}
