07e1b7de5a289653aec22a19f9b9a73b6375a886
tkkrr <reioffitial@gmail.com>
2021-03-17 18:32:25 +0900

    first step

--- SearchExcluder Extension/Resources/content.js
+++ SearchExcluder Extension/Resources/content.js
@@ -1,7 +1,17 @@
-browser.runtime.sendMessage({ greeting: "hello" }).then((response) => {
-    console.log("Received response: ", response);
-});
+const search_result = document.querySelectorAll(".g")
 
-browser.runtime.onMessage.addListener((request, sender, sendResponse) => {
-    console.log("Received request: ", request);
-});
+// exclude items from search list
+Array.from(search_result).forEach( async (item) => {
+    const sample = item.querySelector("a").href
+    const basename = sample.slice(sample.search(/\/\/(.+)/)+2).split("/")[0]
+    const {exclude} = await browser.storage.local.get()
+
+    for(let i of exclude){
+        if(basename.includes(i)){
+            item.style = "display: none;"
+        }
+    }
+})
+
+// exclude Ads item
+document.getElementById("taw").style = "display: none;"
--- SearchExcluder Extension/Resources/popup.js
+++ SearchExcluder Extension/Resources/popup.js
@@ -1,1 +1,19 @@
-console.log("Hello World!", browser);
+window.addEventListener("DOMContentLoaded", async () => {
+    const sample = document.querySelector("textarea.excludelist")
+    const submitButton = document.querySelector("button.submit")
+    
+    // added exclude item
+    submitButton.addEventListener("click", event => {
+        const excludelist = sample.value.split("\n")
+        browser.storage.local.set({
+            exclude: excludelist
+        })
+        console.log("exclude list updated!")
+    })
+    
+    // get exsisted exclude list
+    const result = await browser.storage.local.get()
+    if(!result) return
+    sample.value = result.exclude.join("\n")
+})
+
